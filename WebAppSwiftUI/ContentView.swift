1e2efd8a105d5e78fd42b2e59ea374ad8ca267c2
jan-zelaznog <jan.zelaznog@gmail.com>
2022-05-31 20:44:46 -0500

    Initial Commit

---
+++ WebAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebAppSwiftUI
+//
+//  Created by Jan Zelaznog on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebAppSwiftUI/WebAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebAppSwiftUIApp.swift
+//  WebAppSwiftUI
+//
+//  Created by Jan Zelaznog on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WebAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
